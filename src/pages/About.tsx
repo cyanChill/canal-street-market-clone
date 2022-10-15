5c89768483a7f97c8374e2432084fb65ba90bc94
cyanChill <anthonyliang9@gmail.com>
2022-10-14 22:35:11 -0400

    add class for animated border on hover + market location in about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import styles from "./About.module.css";
 import { HomePageCards, HomeEvents } from "../data";
 import useObserver from "../hooks/useObserver";
+import Contact from "../components/contact/Contact";
 
 const AboutPage = () => {
   return (
@@ -16,13 +17,13 @@ const AboutPage = () => {
 
       <img src="/assets/hero.jpg" alt="" className={styles.hero} />
 
+      {/* Market Info */}
       <h2
         className={`${styles.headingText} ${styles.secondaryHeading}`}
         style={{ "--font-size": "8em" } as React.CSSProperties}
       >
         A New Kind of Market
       </h2>
-
       <div className={styles.cardsContainer}>
         {HomePageCards.map((info) => (
           <div key={info.id} className={styles.card}>
@@ -32,12 +33,12 @@ const AboutPage = () => {
         ))}
       </div>
 
+      {/* Events stuff */}
       <div className={styles.eventsHeading}>
         <span>活動</span>
         <span>Market Events</span>
         <span>活動</span>
       </div>
-
       <div className={styles.eventList}>
         {HomeEvents.map((event) => (
           <p className={styles.event}>
@@ -46,9 +47,21 @@ const AboutPage = () => {
             {event.description}
           </p>
         ))}
-
-        <button className={`btn ${styles.eventBtn}`}>see all</button>
+        <button className={`btn filled ${styles.eventBtn}`}>see all</button>
       </div>
+
+      {/* Canal St. Market Location */}
+      <div className={styles.locationContainer}>
+        <div className={`hoverBorder ${styles.locationTxt}`}>
+          <span>265 Canal St. New York, NY</span>
+        </div>
+        <div className={`hoverBorder ${styles.locationImg}`}>
+          <img src="/assets/location_map.jpg" alt="" />
+        </div>
+      </div>
+
+      {/* Contact Section */}
+      <Contact />
     </div>
   );
 };
